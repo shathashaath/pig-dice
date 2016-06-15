517dac7334111b55f5d95adc5d28f837146f5150
Shatha Shaath and Shradha Pulla <shatha@>
2016-06-15 10:08:37 -0700

    Add dice value box, format in css

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,18 @@
 //Business End Logic
+function Players (totalScore, turnScore) {
+  this.totalScore = totalScore;
+  this.turnScore = turnScore;
+}
 
+Players.prototype.rollDice = function (diceValue) {
+  var dicePlayer1 = Math.floor(Math.random() * 6) + 1;
+  var dicePlayer2 = Math.floor(Math.random() * 6) + 1;
+}
 
 
 
 //User Interface Logic
 $(document).ready(function() {
+  
 
 });
