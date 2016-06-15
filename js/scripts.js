85ccb3c4ec321ce0a8d8e7fefb8c8001441226d8
Shatha Shaath and Shradha Pulla <shradha@>
2016-06-15 11:02:21 -0700

    Make roll dice btn work and showdice value in center square

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,41 @@
-//Business End Logic
+// Business End Logic
 function Players (totalScore, turnScore) {
   this.totalScore = totalScore;
   this.turnScore = turnScore;
 }
 
-Players.prototype.rollDice = function (diceValue) {
+Players.prototype.totalScore = function (score) {
+
+}
+// var numbers=[1,2,3,4,5,6]
+
+function rollDice() {
+  var p1Roll = document.getElementById("die1");
   var dicePlayer1 = Math.floor(Math.random() * 6) + 1;
-  var dicePlayer2 = Math.floor(Math.random() * 6) + 1;
+  p1Roll.innerHTML = dicePlayer1;
 }
 
 
 
-//User Interface Logic
+// // User Interface Logic
 $(document).ready(function() {
-  
+  event.preventDefault();
+
+  $("#p1-roll").click(function(event) {
+    event.preventDefault();
+    $("#dice1").show();
+  });
+  $("#p1-hold").click(function(event) {
+    event.preventDefault();
+
+  });
+  $("#p2-roll").click(function(event) {
+    event.preventDefault();
+
+  });
+  $("#p2-hold").click(function(event) {
+    event.preventDefault();
+
+  });
 
 });
